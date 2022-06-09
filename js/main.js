3841c1f874a2894ae03c46f25854703dc86f83e4
ESariROC <94495713+ESariROC@users.noreply.github.com>
2022-06-10 01:39:11 +0200

    Alles behalve 4 Expert

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ console.log(firstName);
 console.log(lastName);
 
 let fullNameFirst = `${firstName}` + ` ` +`${lastName}`;
-console.log(fullNameFirst);
+console.log(fullNameFirst); 
+// console.log(firstName, lastName);
 
 //opdracht 1 basic 
 
@@ -18,26 +19,32 @@ console.log(secondResult);
 
 //opdracht 1 advanced
 
-function fullName() {
-    const question = prompt('Wat is je voor en achternaam?')
+function firstNamePrompt() {
+    const question = prompt('Wat is je voornaam?')
     const answer = question.charAt(0).toUpperCase() + question.slice(1);
     return answer;
 }
 
-console.log(fullName());
+function lastNamePrompt() {
+    const questionSecond = prompt('Wat is je achternaam?')
+    const answerSecond = questionSecond.charAt(0).toUpperCase() + questionSecond.slice(1);
+    return answerSecond;
+}
+console.log(firstNamePrompt());
+console.log(lastNamePrompt());
 
 //opdracht 1 expert
 
-const question = prompt('Voor de dubbelcheck, wat was je voor en achternaam?')
+const question = prompt('Wat was je voor en achternaam?')
 let printName = document.querySelector('.names');
 printName.innerHTML += `${question}`;
 
 //opdracht 2 basic
 
+document.body.innerHTML += "Welkom";
+
 let button = document.querySelector('.button');
 button.addEventListener('click', myFunction)
-
-alert('Welkom bij dit leeg tabblad');
 
 function myFunction() {
     alert(question);
@@ -45,27 +52,46 @@ function myFunction() {
 
 //opdracht 2 advanced
 
+const inputFirstName = document.querySelector('.inputfirstname');
+const inputLastName = document.querySelector('.inputsecondname');
+const buttonName = document.querySelector('.add-item-button');
+const textName = document.querySelector('.textshow');
 
+buttonName.addEventListener('click', showFunction);
+function showFunction() {
+    const valueFirstName = inputFirstName.value;
+    const valueLastName = inputLastName.value;
+    console.log(valueFirstName, valueLastName);
+
+    const valueFirstNameCapital = valueFirstName.charAt(0).toUpperCase() + valueFirstName.slice(1);
+    const valueLastNameCapital = valueLastName.charAt(0).toUpperCase() + valueLastName.slice(1);
+    console.log(valueFirstNameCapital, valueLastNameCapital);
+    textName.innerHTML += `${valueFirstNameCapital}` + ` ` + `${valueLastNameCapital}`;
+}
 
 //opdracht 2 expert
+
+const check = document.querySelector('.check');
+const fail = document.querySelector('.fail');
+const warning = document.querySelector('.warning');
 
 let password = prompt('Geef een wachtwoord');
 
     if(password == "h@ck3d" || password == "H@ck3D") {
         alert('Je hebt een geldige wachtwoord gegeven');
+        check.classList.remove('disable')
     }
     else if (password == "") {
         alert("You've failed, game over!");
+        warning.classList.remove('disable')
     }
     else {
         alert('Je hebt een verkeerd wachtwoord gegeven');
+        fail.classList.remove('disable')
     };
 
-    //opdracht 3 basic advanced
-
-    const imgTag = document.querySelector(".stock");
-    imgTag.src = "./img/stock.jpg";
-
-    //opdracht 3 expert
+    //opdracht 3 basic advanced expert
 
 
+
+ 
